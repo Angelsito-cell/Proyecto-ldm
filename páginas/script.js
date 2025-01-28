a35ec4ecba7012d3139edba335fa9cc356437379
Angelsito-cell <mgangel@iesrafaelalberti.es>
2025-01-28 17:10:09 +0100

    Terminado la sección de musica

--- páginas/script.js
+++ páginas/script.js
@@ -2,12 +2,14 @@ document.getElementById('search-bar').addEventListener('input', function() {
     let filter = this.value.toLowerCase();
     let albums = document.querySelectorAll('.album');
 
-    albums.forEach(function(album) {
+    albums.forEach((album, index) => {
         let title = album.querySelector('p').textContent.toLowerCase();
         if (title.includes(filter)) {
             album.classList.remove('hidden');
+            album.style.order = 0; // Colocar los elementos coincidentes arriba
         } else {
             album.classList.add('hidden');
+            album.style.order = 1 + index; // Mantener el orden original para los elementos no coincidentes
         }
     });
 });
