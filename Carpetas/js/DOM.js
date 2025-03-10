c1705f641b40e9f32f77ad529e8a8170ec3ae98e
Angelsito-cell <mgangel@iesrafaelalberti.es>
2025-03-10 15:49:22 +0100

    Hecho cositas

---
+++ Carpetas/js/DOM.js
@@ -0,0 +1,20 @@
+
+//get element by id
+
+const footer = document.getElementById("footer");
+console.log(footer);
+
+const head = document.getElementsByTagName("head");
+console.log(head);
+
+const card= document.createElement("formulario");
+console.log(card);
+
+const titulo = document.querySelector("h1");
+console.log(titulo);
+
+const img = document.querySelector("img");
+console.log(img);
+
+
+
